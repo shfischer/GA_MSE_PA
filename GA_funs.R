3d7774f349e0a70172424f2d6b2ed8a68c446900
shfischer <32540427+shfischer@users.noreply.github.com>
2020-06-04 16:23:42 +0100

    flexible fitness function

--- GA_funs.R
+++ GA_funs.R
@@ -127,6 +127,9 @@ mse_r <- function(params, input, path, check_file = FALSE,
 mse_ms <- function(params, inp_file, path, check_file = FALSE,
                    scenario, 
                    return_res = FALSE,
+                   collapse_correction = TRUE,
+                   obj_SSB = TRUE, obj_C = TRUE, obj_F = FALSE,
+                   obj_risk = TRUE, obj_ICV = TRUE,
                    ...) {
   
   ### housekeeping
@@ -152,104 +155,115 @@ mse_ms <- function(params, inp_file, path, check_file = FALSE,
     if (!dir.exists(path)) dir.create(path, recursive = TRUE)
     ### check if run already exists
     if (isTRUE(file.exists(paste0(path, run_i, ".rds")))) {
+      ### load stats
       stats <- readRDS(paste0(path, run_i, ".rds"))
-      ### if run already exists, return value and quit
-      return(stats$obj)
+      ### set flag for running MP
+      run_mp <- FALSE
+    } else {
+      run_mp <- TRUE
     }
+  } else {
+    run_mp <- TRUE
   }
   
-  ### load input file from disk
-  input <- readRDS(inp_file)
-  
-  ### insert arguments into input object for mp
-  input <- lapply(input, function(x) {
-    x$ctrl.mp$ctrl.est@args$idxB_lag     <- params[1]
-    x$ctrl.mp$ctrl.est@args$idxB_range_1 <- params[2]
-    x$ctrl.mp$ctrl.est@args$idxB_range_2 <- params[3]
-    x$ctrl.mp$ctrl.est@args$catch_range  <- params[4]
-    x$ctrl.mp$ctrl.phcr@args$exp_r <- params[5]
-    x$ctrl.mp$ctrl.phcr@args$exp_f <- params[6]
-    x$ctrl.mp$ctrl.phcr@args$exp_b <- params[7]
-    x$ctrl.mp$ctrl.hcr@args$interval <- params[8]
-    x$ctrl.mp$ctrl.phcr@args$multiplier <- params[9]
-    return(x)
-  })
-  
-  ### run MP for each list element
-  res_mp <- lapply(input, function(x) {
-    if (getDoParWorkers() > 1)
-      . <- foreach(i = 1:getDoParWorkers()) %dopar% {invisible(gc())}
-    do.call(mpDL, x)
-  })
-  
-  if (isTRUE(return_res)) {
-    return(res_mp)
+  if (isTRUE(run_mp)) {
+    
+    ### load input file from disk
+    input <- readRDS(inp_file)
+    
+    ### insert arguments into input object for mp
+    input <- lapply(input, function(x) {
+      x$ctrl.mp$ctrl.est@args$idxB_lag     <- params[1]
+      x$ctrl.mp$ctrl.est@args$idxB_range_1 <- params[2]
+      x$ctrl.mp$ctrl.est@args$idxB_range_2 <- params[3]
+      x$ctrl.mp$ctrl.est@args$catch_range  <- params[4]
+      x$ctrl.mp$ctrl.phcr@args$exp_r <- params[5]
+      x$ctrl.mp$ctrl.phcr@args$exp_f <- params[6]
+      x$ctrl.mp$ctrl.phcr@args$exp_b <- params[7]
+      x$ctrl.mp$ctrl.hcr@args$interval <- params[8]
+      x$ctrl.mp$ctrl.phcr@args$multiplier <- params[9]
+      return(x)
+    })
+    
+    ### run MP for each list element
+    res_mp <- lapply(input, function(x) {
+      if (getDoParWorkers() > 1)
+        . <- foreach(i = 1:getDoParWorkers()) %dopar% {invisible(gc())}
+      do.call(mpDL, x)
+    })
+    
+    if (isTRUE(return_res)) {
+      return(res_mp)
+    }
+    
+    ### calculate stats
+    stats <- mapply(function(input_i, res_mp_i) {
+      
+      ### stock metrics
+      SSBs <- FLCore::window(ssb(res_mp_i@stock), start = 101)
+      Fs <- FLCore::window(fbar(res_mp_i@stock), start = 101)
+      Cs <- FLCore::window(catch(res_mp_i@stock), start = 101)
+      yrs <- dim(SSBs)[2]
+      its <- dim(SSBs)[6]
+      ### collapse correction
+      if (isTRUE(collapse_correction)) {
+        ### find collapses
+        cd <- sapply(seq(its), function(x) {
+          min_yr <- min(which(SSBs[,,,,, x] < 1))
+          if (is.finite(min_yr)) {
+            all_yrs <- min_yr:yrs
+          } else {
+            all_yrs <- NA
+          }
+          all_yrs + (x - 1)*yrs
+        })
+        cd <- unlist(cd)
+        cd <- cd[which(!is.na(cd))]
+        ### remove values
+        SSBs@.Data[cd] <- 0
+        Cs@.Data[cd] <- 0
+        Fs@.Data[cd] <- 0
+      }
+      
+      Bmsy <- c(input_i$refpts["msy", "ssb"])
+      Fmsy <- c(input_i$refpts["msy", "harvest"])
+      Cmsy <- c(input_i$refpts["msy", "yield"])
+      Blim <- input_i$Blim
+      ### TAC interval
+      TAC_intvl <- input_i$ctrl.mp$ctrl.hcr@args$interval
+      
+      ### some stats
+      stats_i <- list(
+        risk_Blim = mean(c(SSBs < Blim), na.rm = TRUE),
+        risk_Bmsy = mean(c(SSBs < Bmsy), na.rm = TRUE),
+        risk_halfBmsy = mean(c(SSBs < Bmsy/2), na.rm = TRUE),
+        risk_collapse = mean(c(SSBs < 1), na.rm = TRUE),
+        SSB = median(c(SSBs), na.rm = TRUE), Fbar = median(c(Fs), na.rm = TRUE),
+        Catch = median(c(Cs), na.rm = TRUE),
+        SSB_rel = median(c(SSBs/Bmsy), na.rm = TRUE),
+        Fbar_rel = median(c(Fs/Fmsy), na.rm = TRUE),
+        Catch_rel = median(c(Cs/Cmsy), na.rm = TRUE),
+        ICV = iav(catch(res_mp_i@stock), from = 100, period = TAC_intvl,
+                  summary_all = median)
+      )
+      return(stats_i)
+    }, input, res_mp)
+    ### save result in file
+    if (isTRUE(check_file)) {
+      saveRDS(stats, paste0(path, run_i, ".rds"))
+    }
+    
   }
   
-  ### some quants
-  stats <- mapply(function(input_i, res_mp_i) {
-    
-    SSBs <- FLCore::window(ssb(res_mp_i@stock), start = 101)
-    Fs <- FLCore::window(fbar(res_mp_i@stock), start = 101)
-    Cs <- FLCore::window(catch(res_mp_i@stock), start = 101)
-    Bmsy <- c(input_i$refpts["msy", "ssb"])
-    Fmsy <- c(input_i$refpts["msy", "harvest"])
-    Cmsy <- c(input_i$refpts["msy", "yield"])
-    Blim <- input_i$Blim
-    ### TAC interval
-    TAC_intvl <- input_i$ctrl.mp$ctrl.hcr@args$interval
-    ### SSB/F/Catch objectives
-    SSB_obj <- median(abs(c(SSBs/Bmsy - 1)), na.rm = TRUE)
-    F_obj   <- median(abs(c(Fs/Fmsy - 1)), na.rm = TRUE)
-    C_obj   <- median(abs(c(Cs/Cmsy - 1)), na.rm = TRUE)
-    ### risk, use Blim
-    # risk_obj <- mean(c(SSBs < Bmsy/2), na.rm = TRUE)
-    risk_obj <- mean(c(SSBs < Blim), na.rm = TRUE)
-    ### ICV
-    icv_obj <- iav(catch(res_mp_i@stock), from = 100, period = TAC_intvl,
-                summary_all = median)
-    ### objective function
-    ### add components
-    ### negative because GA maximises function
-    # obj <- -(SSB_obj + F_obj + C_obj + risk_obj + icv_obj)
-    obj <- -(SSB_obj + risk_obj + icv_obj)
-    ### combine
-    obj_i <- list(obj = obj,
-                  obj_SSB = SSB_obj,
-                  obj_F = F_obj,
-                  obj_C = C_obj,
-                  obj_risk = risk_obj,
-                  obj_icv = icv_obj)
-    
-    ### some stats
-    stats_i <- list(
-      risk_Blim = mean(c(SSBs < Blim), na.rm = TRUE),
-      risk_Bmsy = mean(c(SSBs < Bmsy), na.rm = TRUE),
-      risk_halfBmsy = mean(c(SSBs < Bmsy/2), na.rm = TRUE),
-      risk_collapse = mean(c(SSBs < 1), na.rm = TRUE),
-      SSB = median(c(SSBs), na.rm = TRUE), Fbar = median(c(Fs), na.rm = TRUE),
-      Catch = median(c(Cs), na.rm = TRUE),
-      SSB_rel = median(c(SSBs/Bmsy), na.rm = TRUE),
-      Fbar_rel = median(c(Fs/Fmsy), na.rm = TRUE),
-      Catch_rel = median(c(Cs/Cmsy), na.rm = TRUE),
-      ICV = icv_obj
-
-    )
-
-    ### combine
-    res_i <- c(obj_i, stats_i)
-
-    
-  }, input, res_mp)
-  
-  stats <- list(obj = sum(unlist(stats["obj", ])),
-                stats = stats)
-  
-  
-  ### save result in file
-  if (isTRUE(check_file)) {
-    saveRDS(stats, paste0(path, run_i, ".rds"))
-  }
+  ### objective function
+  obj <- 0
+  if (isTRUE(obj_SSB)) obj <- obj + sum(abs(unlist(stats["SSB_rel", ]) - 1))
+  if (isTRUE(obj_C)) obj <- obj + sum(abs(unlist(stats["Catch_rel", ]) - 1))
+  if (isTRUE(obj_F)) obj <- obj + sum(abs(unlist(stats["Fbar_rel", ]) - 1))
+  if (isTRUE(obj_risk)) obj <- obj + sum(unlist(stats["risk_Blim", ]))
+  if (isTRUE(obj_ICV)) obj <- obj + sum(unlist(stats["ICV", ]))
+  ### negative because GA maximises function
+  obj <- -obj
   
   ### housekeeping
   rm(res_mp, input)
@@ -257,7 +271,7 @@ mse_ms <- function(params, inp_file, path, check_file = FALSE,
   if (getDoParWorkers() > 1)
     . <- foreach(i = 1:getDoParWorkers()) %dopar% {invisible(gc())}
   
-  ### return MSE object or fitness value?
-  return(stats$obj)
+  ### return objective function (fitness) value
+  return(obj)
   
 }
