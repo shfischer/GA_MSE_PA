c90e0fa6e00b6ad0fe6f5945b94214963042c47f
shfischer <32540427+shfischer@users.noreply.github.com>
2021-02-24 14:36:18 +0000

    enable sensitivity runs for recruitment variability and observation uncertainty

--- run_ms_hr.R
+++ run_ms_hr.R
@@ -9,9 +9,50 @@ print(args)
 ### evaluate arguments passed to R
 for (i in seq_along(args)) eval(parse(text = args[[i]]))
 
-if (!exists("saveMP")) saveMP <- TRUE
-if (!exists("stats")) stats <- TRUE
-if (!exists("collate")) collate <- FALSE
+### evaluate arguments, if they are passed to R:
+if (length(args) > 0) {
+  
+  ### parallelization
+  if (!exists("use_MPI")) use_MPI <- FALSE
+  if (!exists("n_blocks")) n_blocks <- 1
+  if (!exists("n_workers")) n_workers <- 0
+  
+  ### split OM into blocks?
+  if (!exists("n_parts")) n_parts <- 1
+  if (!exists("part")) part <- 1
+  
+  ### projection details
+  if (!exists("n_iter")) n_iter <- 500
+  if (!exists("n_yrs")) n_yrs <- 50
+  if (!exists("fhist")) fhist <- "random"
+  
+  ### MP parameters
+  if (!exists("hr")) hr <- "length"
+  if (!exists("multiplier")) multiplier <- 1
+  if (!exists("comp_b")) comp_b <- TRUE
+  if (!exists("interval")) interval <- 1
+  if (!exists("idxB_lag")) idxB_lag <- 1
+  if (!exists("idxB_range_3")) idxB_range_3 <- 1
+  if (!exists("upper_constraint")) upper_constraint <- Inf
+  if (!exists("lower_constraint")) lower_constraint <- 0
+  if (!exists("cap_below_b")) cap_below_b <- TRUE
+  
+  if (!exists("stat_yrs")) stat_yrs <- "all"
+  if (!exists("scenario")) scenario <- "sensitivity"
+  
+  ### observation uncertainty
+  if (!exists("sigmaL")) sigmaL <- 0.2
+  if (!exists("sigmaB")) sigmaB <- 0.2
+  ### recruitment variability
+  if (!exists("sigmaR")) sigmaR <- 0.6
+  if (!exists("sigmaR_rho")) sigmaR_rho <- 0.0
+  
+  ### what to save
+  if (!exists("saveMP")) saveMP <- TRUE
+  if (!exists("stats")) stats <- TRUE
+  if (!exists("collate")) collate <- FALSE
+
+}
 
 ### ------------------------------------------------------------------------ ###
 ### set up environment ####
@@ -60,32 +101,27 @@ input$args$nblocks <- n_blocks
 ### MP parameters ####
 ### ------------------------------------------------------------------------ ###
 
-### default values
-if (!exists("hr")) hr <- "uniform"
-if (!exists("multiplier")) multiplier <- 1
-if (!exists("comp_b")) comp_b <- FALSE
-if (!exists("interval")) interval <- 1
-if (!exists("idxB_lag")) idxB_lag <- 1
-if (!exists("idxB_range_3")) idxB_range_3 <- 1
-if (!exists("upper_constraint")) upper_constraint <- Inf
-if (!exists("lower_constraint")) lower_constraint <- 0
-if (!exists("cap_below_b")) cap_below_b <- TRUE
-if (!exists("stat_yrs")) stat_yrs <- "all"
-
 ### load reference values
 hr_ref <- readRDS("input/catch_rates.rds")[[stock]]
 brp <- readRDS("input/brps.rds")[[stock]]
 lhist <- stocks[stocks$stock == stock, ]
 
-### HR rule parameters
+### HR rule parameters & uncertainty
 hr_params <- data.frame(multiplier = multiplier,
                         comp_b = comp_b,
                         idxB_lag = idxB_lag,
                         idxB_range_3 = idxB_range_3,
                         interval = interval,
                         upper_constraint = upper_constraint,
-                        lower_constraint = lower_constraint)
+                        lower_constraint = lower_constraint,
+                        sigmaL = sigmaL,
+                        sigmaB = sigmaB,
+                        sigmaR = sigmaR,
+                        sigmaR_rho = sigmaR_rho)
 
+### ------------------------------------------------------------------------ ###
+### go through runs ####
+### ------------------------------------------------------------------------ ###
 
 if (isTRUE(n_workers > 1 & n_blocks == 1)) {
   `%do_tmp%` <- `%dopar%`
@@ -107,13 +143,114 @@ if (isTRUE(n_workers > 1 & n_blocks == 1)) {
                   lower_constraint = par_i$lower_constraint,
                   cap_below_b = cap_below_b)
   
+  ## ---------------------------------------------------------------------- ###
+  ## observation uncertainty ####
+  ## ---------------------------------------------------------------------- ###
+  ### change uncertainty?
+  sigmaB_i <- par_i$sigmaB
+  sigmaL_i <- par_i$sigmaL
+  if (par_i$sigmaB != 0.2 | par_i$sigmaL != 0.2) {
+    
+    input_i <- lapply(list(input), function(x) {
+      
+      #browser()
+      ### create observation noise
+      set.seed(695)
+      dev_idxB <- x$oem@deviances$idx$idxB
+      dev_idxL <- x$oem@deviances$idx$idxL
+      dev_idxB[] <- rlnoise(n = dims(dev_idxB)$iter, dev_idxB %=% 0, 
+                            sd = sigmaB_i, b = 0)
+      dev_idxL[] <- rlnoise(n = dims(dev_idxL)$iter, dev_idxL %=% 0, 
+                            sd = sigmaL_i, b = 0)
+      set.seed(696)
+      dev_idxB[, ac(50:150)] <- rlnoise(n = dims(dev_idxB)$iter,
+                                        window(dev_idxB, end = 150) %=% 0,
+                                        sd = sigmaB_i, b = 0)
+      dev_idxL[, ac(50:150)] <- rlnoise(n = dims(dev_idxB)$iter,
+                                        window(dev_idxB, end = 150) %=% 0,
+                                        sd = sigmaL_i, b = 0)
+      ### insert
+      x$oem@deviances$idx$idxB <- dev_idxB
+      x$oem@deviances$idx$idxL <- dev_idxL
+      
+      ### update I_trigger
+      I_loss_dev <- apply((x$oem@observations$idx$idxB *
+                             dev_idxB)[, ac(50:100)], 6, min)
+      I_trigger_dev <- I_loss_dev * 1.4
+      x$ctrl$est@args$I_trigger <- c(I_trigger_dev)
+      x$I_loss$idx_dev <- I_loss_dev
+      
+      return(x)
+      
+    })
+    input <- input_i[[1]]
+    
+  }
+  
+  ## ---------------------------------------------------------------------- ###
+  ## recruitment variability ####
+  ## ---------------------------------------------------------------------- ###
+  ### change variability?
+  sigmaR_i <- par_i$sigmaR
+  sigmaR_rho_i <- par_i$sigmaR_rho
+  if (sigmaR_i != 0.6 | sigmaR_rho_i != 0) {
+    
+    input_i <- lapply(list(input), function(x) {
+      
+      #browser()
+      ### retrieve original residuals
+      dev_R_original <- x$om@sr@residuals
+      
+      ### create recruitment residuals for projection period
+      set.seed(1)
+      dev_R_new <- rlnoise(dims(dev_R_original)$iter, dev_R_original %=% 0, 
+                           sd = sigmaR_i, b = sigmaR_rho_i)
+      ### replicate residuals from GA paper
+      qnt_150 <- FLQuant(NA, 
+                         dimnames = list(age = "all", year = 0:150,
+                                         iter = dimnames(dev_R_original)$iter))
+      qnt_100 <- FLQuant(NA, 
+                         dimnames = list(age = "all", year = 1:100,
+                                         iter = dimnames(dev_R_original)$iter))
+      set.seed(0)
+      res_150 <- rlnoise(dims(dev_R_original)$iter,
+                         qnt_150 %=% 0, 
+                         sd = sigmaR_i, b = sigmaR_rho_i)
+      set.seed(0)
+      res_100 <- rlnoise(dims(dev_R_original)$iter,
+                         qnt_100 %=% 0, 
+                         sd = sigmaR_i, b = sigmaR_rho_i)
+      ### insert into template
+      yrs_150 <- seq(from = dims(dev_R_original)$minyear,
+                     to = ifelse(dims(dev_R_original)$maxyear >= 150, 
+                                 150, dims(dev_R_original)$maxyear))
+      dev_R_new[, ac(yrs_150)] <- res_150[, ac(yrs_150)]
+      
+      yrs_100 <- seq(from = dims(dev_R_original)$minyear,
+                     to = 100)
+      dev_R_new[, ac(yrs_100)] <- res_100[, ac(yrs_100)]
+      
+      
+      ### insert
+      x$om@sr@residuals[] <- dev_R_new
+      
+      return(x)
+      
+    })
+    input <- input_i[[1]]
+    
+  }
+  
+  
   ### ---------------------------------------------------------------------- ###
   ### paths ####
   ### ---------------------------------------------------------------------- ###
   ### generate file name
   file_out <- paste0(c(hr, par_i$multiplier, par_i$comp_b, par_i$idxB_lag, 
                        par_i$idxB_range_3, par_i$interval, 
-                       par_i$upper_constraint, par_i$lower_constraint), 
+                       par_i$upper_constraint, par_i$lower_constraint,
+                       par_i$sigmaL, par_i$sigmaB, par_i$sigmaR, 
+                       par_i$sigmaR_rho), 
                      collapse = "_")
   path_out <- paste0("output/hr/", n_iter, "_", n_yrs, "/", scenario, "/",
                      fhist, "/", paste0(stock, collapse = "_"), "/")
@@ -158,14 +295,14 @@ if (isTRUE(stats) & isTRUE(collate) & isTRUE(nrow(hr_params) > 1)) {
                   sapply(seq(nrow(hr_params)), 
                          function(x) paste0(hr_params[x,], collapse = "_")),
                   ".rds")
-  files <- paste0("output/", n_iter, "_", n_yrs, "/", scenario, "/",
+  files <- paste0("output/hr/", n_iter, "_", n_yrs, "/", scenario, "/",
                   fhist, "/", paste0(stock, collapse = "_"), "/",
                   files)
   stats_all <- lapply(files, readRDS)
   stats_all <- do.call(rbind, stats_all)
   
   saveRDS(stats_all, file = paste0(
-    "output/", n_iter, "_", n_yrs, "/", scenario, "/", fhist, "/", 
+    "output/hr/", n_iter, "_", n_yrs, "/", scenario, "/", fhist, "/", 
     paste0(stock, collapse = "_"), "/",
     "collated_stats_", hr, "_", 
     paste0(apply(hr_params, 2, function(x) {
